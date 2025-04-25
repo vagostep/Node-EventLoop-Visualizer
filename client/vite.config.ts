3c9b76edba75f8838f6d6b49b0114cb682de4dff
andres <andres29diaz@gmail.com>
2025-04-24 22:45:21 -0500

    testing with github pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: "https://vagostep.github.io/",
+  base: "https://vagostep.github.io/Node-EventLoop-Visualizer",
   plugins: [react(), tsconfigPaths()],
-})
+});
